3b357b030e1fcfb67274e05cdb0a10a22f24aff3
김다영 (클라라) <kimdayoung771@gmail.com>
2022-05-20 17:49:04 +0900

    design pattern - builder pattern

--- src/app.js
+++ src/app.js
@@ -190,3 +190,34 @@ var Shoe = {
     }
 };
 Shoe.create('boot');
+//--------------------------------------------
+//builder pattern
+var RequestBuilder = /** @class */ (function () {
+    function RequestBuilder() {
+        this.data = null;
+        this.method = null;
+        this.url = null;
+    }
+    RequestBuilder.prototype.setMethod = function (method) {
+        this.method = method;
+        return this;
+    };
+    RequestBuilder.prototype.setData = function (data) {
+        this.data = data;
+        return this;
+    };
+    // this: instance of RequestBuilder
+    RequestBuilder.prototype.setURL = function (url) {
+        this.url = url;
+        return this;
+    };
+    RequestBuilder.prototype.send = function () {
+        //
+    };
+    return RequestBuilder;
+}());
+new RequestBuilder()
+    .setURL('/users')
+    .setMethod('get')
+    .setData({ firstName: 'Anna' })
+    .send();
--- src/app.ts
+++ src/app.ts
@@ -188,5 +188,38 @@ let Shoe = {
 }
 
 Shoe.create('boot')
+//--------------------------------------------
+//builder pattern
+
+class RequestBuilder{
+    private data: object | null = null
+    private method: 'get' | 'post' | null = null
+    private url: string | null = null
+
+    setMethod(method: 'get' | 'post'): this {
+        this.method = method
+        return this
+    }
+    setData(data: object): this {
+        this.data = data
+        return this
+    }
+    // this: instance of RequestBuilder
+    setURL(url: string): this {
+        this.url = url
+        return this
+    }
+    send() {
+        //
+    }
+}
+
+new RequestBuilder()
+    .setURL('/users')
+    .setMethod('get')
+    .setData({firstName: 'Anna'})
+    .send()
+
+
 
 
