7047ed1fa582ad338845acd32f48804eea02baff
Clara <kimdayoung771@gmail.com>
2023-03-29 21:38:25 +0900

    type

--- src/chapter6.ts
+++ src/chapter6.ts
@@ -26,7 +26,47 @@ let legacyUser: LegacyUser = {
   name: 'Xin Yang'
 }
 
-deleteUser(legacyUser)
+// deleteUser(legacyUser)
 
-deleteUser(existingUser)
-console.log(existingUser.id)
+// deleteUser(existingUser)
+// console.log(existingUser.id)
+
+///////////////////////////////////////////////////////////
+
+class Animal {}
+function chirp(bird: Bird): Bird {
+  bird.chirp()
+  return bird
+}
+class Bird extends Animal {
+  chirp() {}
+}
+
+class Crow extends Bird {
+  caw() {}
+}
+
+chirp(new Crow)
+
+function clone(f: (b: Bird) => Bird): void {
+  let parent = new Bird
+  let babyBird = f(parent)
+  babyBird.chirp()
+}
+
+function birdToBird(b: Bird): Bird {
+  return b
+}
+
+clone(birdToBird)
+
+function birdToCrow(d: Bird): Crow {
+  return d
+}
+clone(birdToCrow)
+
+function birdToAnimal(d: Bird): Animal {
+  return d
+}
+
+clone(birdToAnimal)
